a919c2b1e38c3cbeb6f6bec48b6e287e25b5a1c9
satanas <wil.alejandro@gmail.com>
2019-04-12 16:14:20 -0700

    Adding github module. Fixing progress indicator

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const questions = require('./lib/questions');
 const loc = require('./lib/location');
+const github = require('./lib/github');
 
 module.exports = {
   getEventData: questions.getEventData,
   previewData: questions.previewData,
-  getEventLocationCoordinates: loc.getEventLocationCoordinates
+  getEventLocationCoordinates: loc.getEventLocationCoordinates,
+  createMentorIssue: github.createMentorIssue
 }
