b8bd591683efaa6790773117626fb30dbdf4b0fd
satanas <wil.alejandro@gmail.com>
2019-04-12 17:40:30 -0700

    Adding module to create event on Meetup.com

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const questions = require('./lib/questions');
 const loc = require('./lib/location');
 const github = require('./lib/github');
+const meetup = require('./lib/meetup');
 
 module.exports = {
   getEventData: questions.getEventData,
   previewData: questions.previewData,
   getEventLocationCoordinates: loc.getEventLocationCoordinates,
+  createMeetupEvent: meetup.createMeetupEvent,
   createMentorIssue: github.createMentorIssue
 }
